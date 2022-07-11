2c111e2a0607053e6bb897a3e90baa30f3fd376e
Nikolai Krivoshchapov <knvvv@yandex.ru>
2022-07-11 08:26:45 +0300

    fun tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,30 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <iostream>
+#include <vector>
+
+#include <boost/type_index.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/filesystem.hpp>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j + 1;
-}
-
+using boost::typeindex::type_id_with_cvr;
 namespace py = pybind11;
+
+void add(py::list x) {
+    std::cout << "Argtype = " << type_id_with_cvr<decltype(x)>().pretty_name() << "\n";
+    for(unsigned int i = 0; i < x.size(); ++i ){
+        x[i] = 1;
+        int xxx = x[i].cast<int>();
+        std::cout << xxx << "\n";
+    }
+    x.append(10);
+}
 
 PYBIND11_MODULE(confpool, m) {
     m.def("add", &add);
-
     m.def("subtract", [](int i, int j) { return i - j; });
 }
--- tests/mytest.py
+++ tests/mytest.py
@@ -3,4 +3,6 @@ import confpool as m
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, m.add(1, 2)))
+    x = [1,2,3,4,5]
+    print("{} + {} = {}".format(a, b, m.add(x)))
+    print(repr(x))
